684790c7f8880afad9f8670e2b5eafa1340ead22
Aline Bury <alinebury@gmail.com>
2022-10-10 17:17:32 -0300

    Adicionando projeto no repositorio pessoal

--- src/controllers/Car.Controller.ts
+++ src/controllers/Car.Controller.ts
@@ -8,26 +8,26 @@ export default class CarController {
   public async create(req: Request, res: Response<ICar>) {
     const created = await this._service.create(req.body);
 
-    return res.status(201).json(created);
+    res.status(201).json(created);
   }
 
   public async read(req: Request, res: Response<ICar[]>) {
     const cars = await this._service.read();
 
-    return res.status(200).json(cars);
+    res.status(200).json(cars);
   }
 
   public async readOne(req: Request, res: Response<ICar | null>) {
     const car = await this._service.readOne(req.params.id);
 
-    return res.status(200).json(car);
+    res.status(200).json(car);
   }
 
   public async update(req: Request, res: Response<ICar | null>) {
     const { id } = req.params;
     const updated = await this._service.update(id, req.body);
 
-    return res.status(200).json(updated);
+    res.status(200).json(updated);
   }
 
   public async delete(req: Request, res: Response<ICar | null>) {
@@ -35,6 +35,6 @@ export default class CarController {
 
     const deleted = await this._service.delete(id);
 
-    return res.status(204).json(deleted);
+    res.status(204).json(deleted);
   }
 }
